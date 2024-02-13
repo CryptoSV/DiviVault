98e9b2ac1638841a196d9f72ee6ed11400768909
CryptoSV <154707074+CryptoSV@users.noreply.github.com>
2024-02-13 13:35:36 -0500

    Update notificationsFilesFromGitHub.sh

--- notificationsFilesFromGitHub.sh
+++ notificationsFilesFromGitHub.sh
@@ -13,6 +13,7 @@ read -n 1 -s -r -p "Press spacebar to continue..."
 
 wget -P /home/admin/scripts/notification https://raw.githubusercontent.com/CryptoSV/DiviVault/main/current.sh
 wget -P /home/admin/scripts/notification https://raw.githubusercontent.com/CryptoSV/DiviVault/main/allVaultRewards.sh
+
 chmod +x /home/admin/scripts/notification/*
 
 echo ""
