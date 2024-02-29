29dd52c99af31cab3b315ffcb1f797dbce0b98dd
CryptoSV <154707074+CryptoSV@users.noreply.github.com>
2024-02-29 11:55:15 -0500

    Update getCurrentVaultValue.sh

--- getCurrentVaultValue.sh
+++ getCurrentVaultValue.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# This script gets the current staked value, formats it, and works with New Relic.
+# This scripts parses the last "stake_reward" entry from listtransactions and checks the confirmations. If they are greater than 20 (about 20 minutes), then get current coin availabile and report it to New Relic.
 
-# Run the command and capture the JSON output
-json_output=$(/home/admin/divi-3.0.0/bin/divi-cli getcoinavailability true)
+# Get the JSON output from this command
+output=$(/home/admin/divi-3.0.0/bin/divi-cli listtransactions)
 
-# Extract the value using jq
-value=$(echo "$json_output" | jq -r '.Stakable.AllVaults[0].value')
+# Load the last entry
+last_entry=$(echo "$output" | jq '.[] | select(.category == "stake_reward" and .confirmations > 20) | .[-1]')
 
-# Format the value with thousands commas
-formatedValue=$(/home/admin/scripts/nicenumber.sh $value)
+echo "Last entry: $last_entry"
 
-echo "$formatedValue $value"
+# If last_entry is not empty, echo "done"
+if [ ! -z "$last_entry" ]; then
+	# Run the command and capture the JSON output
+	json_output=$(/home/admin/divi-3.0.0/bin/divi-cli getcoinavailability true)
+
+	# Extract the value using jq
+	value=$(echo "$json_output" | jq -r '.Stakable.AllVaults[0].value')
+
+	# Format the value with thousands commas
+	formatedValue=$(/home/admin/scripts/nicenumber.sh $value)
+
+	echo "$formatedValue $value"
+fi
