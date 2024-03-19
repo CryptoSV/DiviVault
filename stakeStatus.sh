51ae4aa3bb1484b70bea0d3da8aacecaa4cfa429
CryptoSV <154707074+CryptoSV@users.noreply.github.com>
2024-03-19 12:41:04 -0400

    Update stakeStatus.sh
    Added a divi-cli path to make it easier to update.

--- stakeStatus.sh
+++ stakeStatus.sh
@@ -2,7 +2,9 @@
 
 ## Get the staking status
 
+divi_cli="/home/admin/divi-3.0.0/bin/divi-cli"
+
 # Run the Get Staking Status command and load the output into an array
-readarray -t arr < <(/home/admin/divi-3.0.0/bin/divi-cli getstakingstatus | jq -r '.validtime, .haveconnections, .walletunlocked, .mintablecoins, .enoughcoins, .mnsync, ."staking status"')
+readarray -t arr < <("$divi_cli" getstakingstatus | jq -r '.validtime, .haveconnections, .walletunlocked, .mintablecoins, .enoughcoins, .mnsync, ."staking status"')
 
 echo "${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]} ${arr[5]} ${arr[6]}"
